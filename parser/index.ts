17adb792ac16172e172a59f31f04eb668b00d9d7
Germain Rullier <grullier@free.fr>
2023-04-12 05:15:59 +0000

    parser

---
+++ parser/index.ts
@@ -0,0 +1,25 @@
+import Ajv from "ajv"
+import config from "../config.json"
+import data from "../data.json"
+import configSchema from "../schemas/config.json"
+import dataSchema from "../schemas/data.json"
+
+const parser = new Ajv({allErrors: true})
+
+const validConfig = parser.validate(configSchema, config)
+if(!validConfig) {
+    console.log('Config errors:')
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    for (const error of parser.errors!) {
+        console.log(error)
+    }
+}
+
+const validData = parser.validate(dataSchema, data)
+if(!validData) {
+    console.log('Data errors:')
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    for (const error of parser.errors!) {
+        console.log(error)
+    }
+}
