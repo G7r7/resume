767c919224656acffc2504bce448096c085ec5f9
Germain Rullier <grullier@free.fr>
2023-04-15 11:08:07 +0000

    dynamic and robust json parser

--- parser/index.ts
+++ parser/index.ts
@@ -1,8 +1,58 @@
 import Ajv from "ajv"
-import config from "../config.json"
-import data from "../data.json"
 import configSchema from "../schemas/config.json"
 import dataSchema from "../schemas/data.json"
+import fs from "fs"
+
+const usage = 'usage: parser <config-path> <data-path>'
+
+if (process.argv.length != 4) {
+    console.log(usage)
+    process.exit(1)
+}
+
+console.log(process.cwd())
+
+const configPath = process.argv[2]
+const dataPath = process.argv[3]
+
+// We read the configuration and data files
+let configRaw: string
+let dataRaw: string
+
+try {
+    configRaw = fs.readFileSync(configPath).toString()
+} catch (err) {
+    console.log("Can't read config file")
+    console.log(err.message)
+    process.exit(1)
+}
+
+try {
+    dataRaw = fs.readFileSync(dataPath).toString()
+} catch (err) {
+    console.log("Can't read data file")
+    console.log(err.message)
+    process.exit(1)
+}
+
+let config: JSON
+let data: JSON
+
+try {
+    config = JSON.parse(configRaw)
+} catch (error) {
+    console.log("Can't parse config file as JSON")
+    console.log(error.message)
+    process.exit(1)
+}
+
+try {
+    data = JSON.parse(dataRaw)
+} catch (error) {
+    console.log("Can't parse data file as JSON")
+    console.log(error.message)
+    process.exit(1)
+}
 
 const parser = new Ajv({allErrors: true})
 
